d38bc9a87f124af9e45a5ed47030089f64a74eac
Evan Fox <78891786+EvnFox@users.noreply.github.com>
2024-07-30 09:54:37 -0400

    Add files via upload

--- mip_fast.py
+++ mip_fast.py
@@ -1,25 +1,15 @@
-
 from PIL import Image 
 import numpy as np 
 import time 
 
 start_time = time.time()
 
-# Get image and static mask
-pth = "002-001-2023-11-27_17-37-00_IMG-TLP.jpg"
+pth = "minion_pics/002-001-2023-11-27_17-37-00_IMG-TLP.jpg" 
     
-    
-sm = np.array(Image.open('static_mask.png'))
-sm = sm.astype('uint8')
-im0 = np.array(Image.open(pth))
-
-
-#apply static mask to image. 
-im1 = im0[:,:,0]*sm
-  
-im = im1 > 50
-im = im.astype('uint8')
-
+sm = np.array(Image.open('static_mask.png')).astype('uint8')
+im0 = np.array(Image.open(pth)).astype('uint8')
+ 
+im = im0[:,:,0]*sm > 60
 
 print(im.sum())
 
