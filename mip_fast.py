cdd2c43952f802dbfc067924e2a4b0d34b7dc1fb
Evan Fox <78891786+EvnFox@users.noreply.github.com>
2024-07-30 09:55:41 -0400

    Update mip_fast.py

--- mip_fast.py
+++ mip_fast.py
@@ -4,7 +4,7 @@ import time
 
 start_time = time.time()
 
-pth = "minion_pics/002-001-2023-11-27_17-37-00_IMG-TLP.jpg" 
+pth = "002-001-2023-11-27_17-37-00_IMG-TLP.jpg" 
     
 sm = np.array(Image.open('static_mask.png')).astype('uint8')
 im0 = np.array(Image.open(pth)).astype('uint8')
